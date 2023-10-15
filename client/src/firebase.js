9db79dfb748bd32977e74727dece502d2d593532
Sagar Arora <sagararor403@gmail.com>
2023-10-15 11:09:33 +0530

    Jatland Estate Project

---
+++ client/src/firebase.js
@@ -0,0 +1,35 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from 'firebase/app';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-estate.firebaseapp.com',
+//   projectId: 'mern-estate',
+//   storageBucket: 'mern-estate.appspot.com',
+//   messagingSenderId: '1078482850952',
+//   appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-estate-a8428.firebaseapp.com",
+  projectId: "mern-estate-a8428",
+  storageBucket: "mern-estate-a8428.appspot.com",
+  messagingSenderId: "310095502392",
+  appId: "1:310095502392:web:13c00175a0370e58fe1acd",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
